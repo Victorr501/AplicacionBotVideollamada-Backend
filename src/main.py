c97d15d2f3010af960a1c85d7180fa1006a32beb
Víctor Rubín Rubio <vrubinr501@gmail.com>
2026-09-02 14:42:11 +0200

    Actualizacion 9/2/2026

--- src/main.py
+++ src/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from controllers import BotController
 
 app = FastAPI(
       title="API Asistente de Reuniones"
     )
+
+# CONFIGURACIÓN CORS
+# Permite que el frontend pueda hacer solicitudes a la API desde un dominio diferente
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # Permite solicitudes desde cualquier origen aqui tendremos que cambiarlo para el local y azure
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    )
+
+# RUTAS
 
 app.include_router(BotController.router)
 
--- src/services/botServices/BotServices.py
+++ src/services/botServices/BotServices.py
@@ -1,9 +1,48 @@
 from .IBotServices import IBotServices
+from dotenv import load_dotenv
+import requests
+import os
+
+load_dotenv()
 
 class BotServices(IBotServices):
+    def __init__(self):
+        self.api_key_bot = os.getenv("RECALL_API_KEY")
+        self.url_proveedor = "";
+
     def enviar_bot(self, url_reunion: str) -> dict:
         print(f"[SERVICIO] Conectando a bot a: {url_reunion}")
-        return {"status": "ok", "url": url_reunion, "bot_id": "bot_xyz"}
+
+        if not self.api_key_bot:
+            return {"status": "error", "message": "API key no configurada"}
+
+        header = {
+                "Autherization": f"Token {self.api_key_bot}",
+                "Content-Type":"application/json" 
+            }
+
+        payload = {
+                "meeting_url": url_reunion,
+                "bot_name": "Asistente de Reuniones IA"
+            }
+
+        try:
+            respuesta = requests.post(self.url_proveedor, headers=header, json=payload)
+
+            respuesta.raise_for_status()
+
+            datos_bot = respuesta.json()
+
+            return {
+                    "status": "success",
+                    "data": datos_bot.get("id"),
+                    "recall_status": "joining",
+                    "url_reunion": url_reunion
+                }
+
+        except requests.exceptions.RequestException as e:
+            print(f"[ERROR API RECALL] {e}")
+            return {"status": "error", "message": "No se pudo conectar con el proveedor de bots."}
 
     def procesar_audio(self, audio_file: dict) -> dict:
         print(f"[SERVICIO] Procesando archivo de audio...")
